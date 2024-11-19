0d803354ff41d34de2d193ba49cede154f5b6f89
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-11-19 08:18:32 +0000

    PER ORA FUNZIONA TODO 2

--- progettino2.py
+++ progettino2.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 #inizializza l'app Flask
 app = Flask(__name__)
 #rotta principale
 @app.route('/')
 def home():
-    return "PER ORA FUNZIONA TODO"
+    return render_template('index.html')
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
