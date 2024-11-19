0d0f4032fdb61a9cbd69194c18d381baf7551a09
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-11-19 08:40:53 +0000

    PER ORA NON FUNZIONA TODO

--- progettino2.py
+++ progettino2.py
@@ -8,3 +8,17 @@ def home():
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
+
+lista_spesa = ["pane", "cozze"]
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))    
