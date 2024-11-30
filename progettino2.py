ffbc18fa3dfb627e6504c1946abf391d4eb5b0ff
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-11-30 22:00:30 +0000

    FUNZIONA TODO

--- progettino2.py
+++ progettino2.py
@@ -1,43 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
+
+class ListaSpesa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  
+    elemento = db.Column(db.String(100), nullable=False) 
 
 with app.app_context():
-    db.create_all()
-
-app = Flask(__name__)
-
-lista_spesa = []
+    db.create_all()  
 
 @app.route('/')
 def index():
-
-    return render_template('index.html', lista=lista_spesa)
+    elementi = ListaSpesa.query.all()  
+    return render_template('index.html', elementi=elementi)
 
 @app.route('/aggiungi', methods=['POST'])
 def aggiungi():
+    nuovo_elemento = request.form['elemento'] 
+    elemento = ListaSpesa(elemento=nuovo_elemento)
+    db.session.add(elemento)  
+    db.session.commit() 
+    return redirect('/')
 
-    elemento = request.form.get('elemento')
-    if elemento:
-        lista_spesa.append(elemento)
-    return redirect(url_for('index'))
+@app.route('/rimuovi/<int:id>', methods=['POST'])
+def rimuovi(id):
+    elemento = ListaSpesa.query.get_or_404(id)  
+    db.session.delete(elemento)  
+    db.session.commit()  
+    return redirect('/')
 
-@app.route('/rimuovi/<int:indice>')
-def rimuovi(indice):
-
-    if 0 <= indice < len(lista_spesa):
-        lista_spesa.pop(indice)
-    return redirect(url_for('index'))
-
-@app.route('/svuota')
+@app.route('/svuota', methods=['POST'])
 def svuota():
-
-    lista_spesa.clear()
-    return redirect(url_for('index'))
+    ListaSpesa.query.delete() 
+    db.session.commit() 
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
